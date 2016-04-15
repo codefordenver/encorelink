b07f23430beeea3d015c2452e5aee880c596777d
Willy Douglas <willygdouglas@gmail.com>
2016-04-14 20:32:19 -0600

    Set up production database config

---
+++ server/datasources.production.js
@@ -0,0 +1,11 @@
+module.exports = {
+  postgres: {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DB,
+    username: process.env.PG_USER,
+    password: process.env.PG_PW,
+    name: 'postgres',
+    connector: 'postgresql'
+  }
+};
