9443e72b6a96a02711acbee82fd86cd03b8337de
Willy Douglas <wdouglas@rallydev.com>
2016-06-16 19:35:07 -0600

    WIP Create endpoint to create a new meal with associated mealfoods

--- common/models/meal.js
+++ common/models/meal.js
@@ -1,3 +1,29 @@
-module.exports = function(Meal) {
+var _ = require('lodash');
 
+module.exports = function(Meal) {
+  /**
+   * Create a new meal along with associated relations
+   * @param {object} fullMeal The meal object with an array of objects representing the mealfoods
+   * @param {Function(Error, object)} callback
+   */
+
+  Meal.fullMeal = function(fullMeal, callback) {
+    var mealToCreate = _.omit(fullMeal, 'mealfoods');
+    var mealfoodsToCreate = fullMeal.mealfoods;
+
+    var models = Meal.app.models;
+
+    Meal.create(mealToCreate, function(err, meal) {
+      if (err) {
+        return callback(err);
+      }
+      mealfoodsToCreate.forEach(function(mealfoodToCreate) {
+        mealfoodToCreate.mealId = meal.id;
+        models.mealfood.create(mealfoodToCreate, function(err, mealfood) {
+          if (err) console.log('mealfood err', err);
+        });
+      });
+      callback(null, meal);
+    });
+  };
 };
