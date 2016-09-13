e0cb75b5e7b4c2b2e0939b66f3d081552063bdc1
Willy <willygdouglas@gmail.com>
2016-09-12 21:16:34 -0600

    Add organizer and volunteer roles

--- server/boot/sample-models.js
+++ server/boot/sample-models.js
@@ -1,32 +1,74 @@
+var async = require('async');
+
 module.exports = function(app) {
 
   var User = app.models.user;
+  var Role = app.models.Role;
+  var RoleMapping = app.models.RoleMapping;
+
   var users = [
     {
-      username: 'scottsmeester',
+      email: 'organizer@a.bc',
+      password: 'p'
+    },
+    {
+      email: 'musician@a.bc',
+      password: 'p'
+    },
+    {
       email: 'scott@smeester.com',
       password: 'password123'
     },
     {
-      username: 'willydouglas',
       email: 'willy@eastcolfaxcorner.com',
       password: 'password123'
     },
     {
-      username: 'devindeScenza',
       email: 'devin@sketchyalley.com',
       password: 'password123'
     },
     {
-      username: 'ryanbird',
       email: 'ryan@eastaurora.com',
       password: 'password123'
     }
   ];
 
-  User.create(users, function(err, users) {
+  var roles = [{
+    name: 'organizer'
+  }, {
+    name: 'volunteer'
+  }];
+
+  User.create(users, function(err, createdUsers) {
     if (err) throw err;
 
-    console.log('Created users:', users);
+    console.log('Created users:', createdUsers);
+
+    //create the admin role
+    Role.create(roles, function(err, createdRoles) {
+      if (err) throw err;
+
+      console.log('Created roles:', createdRoles);
+
+      // add roles
+      async.each(
+        [0,1],
+
+        function(id, cb) {
+          createdRoles[id].principals.create({
+            principalType: RoleMapping.USER,
+            principalId: users[id].id
+          }, function(err, principal) {
+            cb(err);
+          });
+        },
+
+        function(err) {
+          if (err) throw err;
+
+          console.log('assigned roles');
+        }
+      );
+    });
   });
 };
