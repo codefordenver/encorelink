fe6da8b4dd5069b62bc7a40e75d2e29c2b75acb3
Agustin Vijoditz <avijoditz@wondersign.com>
2016-10-13 21:01:39 -0600

    sample events and admin user created

--- server/boot/sample-models.js
+++ server/boot/sample-models.js
@@ -1,7 +1,15 @@
-module.exports = function(app) {
+module.exports = function (app) {
 
+  /**
+   * User creation
+   */
   var User = app.models.user;
   var users = [
+    {
+      username: 'admin',
+      email: 'admin@encorelink.com',
+      password: 'password123'
+    },
     {
       username: 'scottsmeester',
       email: 'scott@smeester.com',
@@ -24,9 +32,55 @@ module.exports = function(app) {
     }
   ];
 
-  User.create(users, function(err, users) {
+  User.create(users, function (err, users) {
     if (err) throw err;
 
     console.log('Created users:', users);
+  });
+
+  /**
+   * Event creation
+   */
+  var Event = app.models.Event;
+  var events = [
+    {
+      "name": "event 1",
+      "date": "2016-12-22T14:00:00.000Z",
+      "notes": "sample note",
+      "location": "east colfax corner",
+      "endDate": "2016-12-22T15:00:00.000Z",
+      "userId": 1
+    },
+    {
+      "name": "event 2",
+      "date": "2017-01-22T14:00:00.000Z",
+      "notes": "sample note",
+      "location": "east colfax corner",
+      "endDate": "2017-01-22T15:00:00.000Z",
+      "userId": 1
+    },
+    {
+      "name": "event 3",
+      "date": "2016-11-22T14:00:00.000Z",
+      "notes": "sample note",
+      "location": "east colfax corner",
+      "endDate": "2016-11-22T15:00:00.000Z",
+      "userId": 1
+    },
+    {
+      "name": "event 4",
+      "date": "2016-12-23T14:00:00.000Z",
+      "notes": "sample note",
+      "location": "east colfax corner",
+      "endDate": "2016-12-23T15:00:00.000Z",
+      "userId": 1
+    },
+
+  ];
+
+  Event.create(events, function (err, events) {
+    if (err) throw err;
+
+    console.log('Created events:', events);
   });
 };
