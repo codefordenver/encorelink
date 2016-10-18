3a49b83d8d656734f1f4a3e34d5db79f0d76522c
Agustin Vijoditz <avijoditz@wondersign.com>
2016-10-17 19:17:47 -0600

    logout redirects to landing // header minor update
    viet events

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,8 +1,34 @@
 import React, { PropTypes } from 'react';
 import { withRouter, Link } from 'react-router';
 
-class Events extends React.Component {
+class EventRow extends React.Component {
+  static propTypes = {
+    event: PropTypes.shape({
+      date: PropTypes.string.isRequired
+    }).isRequired
+  }
 
+  render() {
+    return (
+      <div className="row">
+        <div className="small-4 columns">
+          {this.props.event.date};
+        </div>
+        <div className="small-4 columns">
+          {this.props.event.endDate}
+        </div>
+        <div className="small-2 columns">
+          {this.props.event.name}
+        </div>
+        <div className="small-2 columns">
+          {this.props.event.location}
+        </div>
+      </div>
+    );
+  }
+} 
+
+class Events extends React.Component {
   static propTypes = {
     loadEvents: PropTypes.func.isRequired,
     events: PropTypes.array.isRequired
@@ -18,13 +44,24 @@ class Events extends React.Component {
 
   render() {
     const events = this.props.events.map(event =>
-      <div key={event.id}>{ event.name }, { event.date }, { event.endDate }, { event.location }, { event.notes }</div>
+      <EventRow key={event.id} event={event} />
     );
     return (
       <div className="volunteer-view-events">
-        <h3>Events</h3>
-        <Link to="/createEvent">Create Event</Link>
-        { events }
+        <div className="row">
+          <div className="small-12 medium-8 columns">
+            <h3>Events</h3>
+          </div>
+          <div className="small-12 medium-4 columns">
+            <Link to="/createEvent" className="secondary button">Create Event</Link>
+          </div>
+        </div>
+        <div className="row event-list">
+          <div className="small-12 columns">
+            <h4>Available Performance Slots</h4>
+            { events }
+          </div>
+        </div>
       </div>
     );
   }
