8f4a7601cfb7a62955d4a9900e6e4daca8210541
Agustin Vijoditz <avijoditz@wondersign.com>
2016-10-17 19:14:39 -0600

    misc header improvements

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,31 +1,33 @@
 import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 
-function Header({ userId, logoutUser }) {
-  const loggedIn = (
-    <div>
-      {userId}
-      <button className="button" onClick={logoutUser}>Log out</button>
-    </div>
-  );
-  const loggedOut = (
-    <Link className="button" to="/login">Log in</Link>
-  );
-  return (
-    <header className="top-bar">
-      <div className="top-bar-left">
-        <img className="logo-img" src="public/img/encorelink-logo.png" alt="EncoreLink" />
-      </div>
-      <div className="top-bar-right text-right">
-        {userId ? loggedIn : loggedOut}
-      </div>
-    </header>
-  );
-}
+class Header extends React.Component {
+  static propTypes = {
+    userId: PropTypes.number,
+    logoutUser: PropTypes.func.isRequired
+  };
 
-Header.propTypes = {
-  userId: PropTypes.number,
-  logoutUser: PropTypes.func.isRequired
-};
+  render() {
+    const loggedIn = (
+      <div>
+        {this.props.userId}
+        <button className="button" onClick={this.props.logoutUser}>Log out</button>
+      </div>
+    );
+    const loggedOut = (
+      <Link className="button" to="/login">Log in</Link>
+    );
+    return (
+      <header className="top-bar">
+        <div className="top-bar-left">
+          <img className="logo-img" src="public/img/encorelink-logo.png" alt="EncoreLink" />
+        </div>
+        <div className="top-bar-right text-right">
+          {this.props.userId ? loggedIn : loggedOut}
+        </div>
+      </header>
+    );
+  }
+}
 
 export default Header;
