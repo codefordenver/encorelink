208a7988f7b475d31e5af0dc6d44d01ce8172e3f
huy thai <kennyt995@gmail.com>
2016-10-27 20:03:05 -0600

    add proptypes validation

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -5,7 +5,10 @@ import moment from 'moment';
 class EventRow extends React.Component {
   static propTypes = {
     event: PropTypes.shape({
-      date: PropTypes.string.isRequired
+      date: PropTypes.string.isRequired,
+      endDate: PropTypes.object.isRequired,
+      name: PropTypes.string.isRequired,
+      location: PropTypes.string.isRequired
     }).isRequired
   }
 
@@ -18,10 +21,10 @@ class EventRow extends React.Component {
         <div className="small-3 columns">
           {moment(this.props.event.date).format('hh:mm a')}-{moment(this.props.event.endDate).format('hh:mm a')}
         </div>
-        <div className="small-6 columns">
+        <div className="small-5 columns">
           {this.props.event.name} {this.props.event.location}
         </div>
-        <div className="small-1 columns">
+        <div className="small-2 columns">
           <Link to="">details</Link>
         </div>
       </div>
@@ -51,7 +54,7 @@ class Events extends React.Component {
       <div className="volunteer-view-events">
         <div className="row">
           <div className="small-12 medium-8 columns">
-            <h3>Events</h3>
+            <h3>Book Performance</h3>
           </div>
           <div className="small-12 medium-4 columns">
             <Link to="/createEvent" className="secondary button">Create Event</Link>
