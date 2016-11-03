a52754cf478730251a539ba06880067d702f1a33
Willy <willygdouglas@gmail.com>
2016-11-03 10:44:08 -0700

    WIP change the organizer profile to use redux-form

--- client/src/components/OrganizerProfile.js
+++ client/src/components/OrganizerProfile.js
@@ -1,5 +1,5 @@
 import React, { PropTypes } from 'react';
-import { withRouter } from 'react-router';
+import { Field, reduxForm } from 'redux-form';
 
 class OrganizerProfile extends React.Component {
   static propTypes = {
@@ -12,42 +12,68 @@ class OrganizerProfile extends React.Component {
         <div className="small-12 columns">
           <h1>Create Your Profile</h1>
           <h5 className="subheader">
-            Your profile provides information we can share with musicians so they know about you and your organization.
+            Your profile provides information we can share with musicians so
+            they know about you and your organization.
           </h5>
           <br />
           <form className="form-create-event" onSubmit={this.handleFormSubmit}>
             <div className="row">
               <div className="small-12 medium-12 columns">
                 <label>Organization name:
-                  <input type="text" onChange={this.handleOrganizationNameChange} required />
+                  <Field
+                    name="orgName"
+                    component="input"
+                    type="text"
+                    required
+                  />
                 </label>
               </div>
             </div>
             <div className="row">
               <div className="small-12 medium-12 columns">
                 <label>Your name:
-                  <input type="text" onChange={this.handleNameChange} required />
+                  <Field
+                    name="userName"
+                    component="input"
+                    type="text"
+                    required
+                  />
                 </label>
               </div>
             </div>
             <div className="row">
               <div className="small-12 medium-12 columns">
                 <label>Job title:
-                  <input type="text" onChange={this.handleJobTitleChange} required />
+                  <Field
+                    name="job title"
+                    component="input"
+                    type="text"
+                    required
+                  />
                 </label>
               </div>
             </div>
             <div className="row">
               <div className="small-12 medium-12 columns">
                 <label>Address:
-                  <input type="text" onChange={this.handleAddressLine1Change} required />
+                  <Field
+                    name="address"
+                    component="input"
+                    type="text"
+                    required
+                  />
                 </label>
               </div>
             </div>
             <div className="row">
               <div className="small-12 medium-12 columns">
                 <label>City:
-                  <input type="text" onChange={this.handleAddressCityChange} required />
+                  <Field
+                    name="city"
+                    component="input"
+                    type="text"
+                    required
+                  />
                 </label>
               </div>
             </div>
@@ -56,7 +82,12 @@ class OrganizerProfile extends React.Component {
                 <div className="row">
                   <div className="small-12 medium-12 columns">
                     <label>State:
-                      <input type="text" onChange={this.handleAddressStateChange} required />
+                      <Field
+                        name="state"
+                        component="input"
+                        type="text"
+                        required
+                      />
                     </label>
                   </div>
                 </div>
@@ -65,7 +96,12 @@ class OrganizerProfile extends React.Component {
                 <div className="row">
                   <div className="small-12 medium-12 columns">
                     <label>Zipcode:
-                      <input type="text" onChange={this.handleAddressZipChange} required />
+                      <Field
+                        name="zip"
+                        component="input"
+                        type="text"
+                        required
+                      />
                     </label>
                   </div>
                 </div>
@@ -74,7 +110,12 @@ class OrganizerProfile extends React.Component {
             <div className="row">
               <div className="small-12 medium-12 columns">
                 <label>Email:
-                  <input type="text" onChange={this.handleEmailChange} required />
+                  <Field
+                    name="email"
+                    component="input"
+                    type="text"
+                    required
+                  />
                 </label>
               </div>
             </div>
@@ -83,7 +124,12 @@ class OrganizerProfile extends React.Component {
                 <div className="row">
                   <div className="small-12 medium-12 columns">
                     <label>Phone:
-                      <input type="text" onChange={this.handlePhoneChange} required />
+                      <Field
+                        name="phone"
+                        component="input"
+                        type="text"
+                        required
+                      />
                     </label>
                   </div>
                 </div>
@@ -92,7 +138,11 @@ class OrganizerProfile extends React.Component {
                 <div className="row">
                   <div className="small-12 medium-12 columns">
                     <label>Ext:
-                      <input type="text" onChange={this.handlePhoneExtChange} />
+                      <Field
+                        name="ext"
+                        component="input"
+                        type="text"
+                      />
                     </label>
                   </div>
                 </div>
@@ -101,23 +151,57 @@ class OrganizerProfile extends React.Component {
             <div className="row">
               <div className="small-12 medium-12 columns">
                 <label>Best contact method (email is default):
-                  <input type="text" onChange={this.handleBestContatMethodChange} />
+                  <Field
+                    name="bestContact"
+                    component="input"
+                    type="text"
+                  />
                 </label>
               </div>
             </div>
             <div className="row">
               <div className="small-12 medium-12 columns">
                 <label>Special notes:
-                  <textarea onChange={this.handleNotesChange} />
+                  <Field
+                    name="notes"
+                    component="textarea"
+                  />
                 </label>
               </div>
             </div>
             <div className="row">
               <div className="small-12 medium-12 columns">
                 <label>Does the facility have a piano or any other instruments:</label>
-                <input type="radio" name="hasInstruments" onChange={this.handleHasInstrumentsChange} value="piano" id="hasInstrumentsPiano" /><label htmlFor="hasInstrumentsPiano">Piano</label>
-                <input type="radio" name="hasInstruments" onChange={this.handleHasInstrumentsChange} value="no" id="hasInstrumentsNo" /><label htmlFor="hasInstrumentsNo">Not yet</label>
-                <input type="radio" name="hasInstruments" onChange={this.handleHasInstrumentsChange} value="other" id="hasInstrumentsOther" /><label htmlFor="hasInstrumentsOther">Other (please specify in special notes above)</label>
+                <Field
+                  name="hasInstruments"
+                  component="input"
+                  type="radio"
+                  value="piano"
+                  id="hasInstrumentsPiano"
+                />
+                <label htmlFor="hasInstrumentsPiano">
+                  Piano
+                </label>
+                <Field
+                  name="hasInstruments"
+                  component="input"
+                  type="radio"
+                  value="no"
+                  id="hasInstrumentsNo"
+                />
+                <label htmlFor="hasInstrumentsNo">
+                  Not yet
+                </label>
+                <Field
+                  name="hasInstruments"
+                  component="input"
+                  type="radio"
+                  value="other"
+                  id="hasInstrumentsOther"
+                />
+                <label htmlFor="hasInstrumentsOther">
+                  Other (please specify in special notes above)
+                </label>
 
               </div>
             </div>
@@ -136,5 +220,6 @@ class OrganizerProfile extends React.Component {
   }
 }
 
-
-export default withRouter(OrganizerProfile);
+export default reduxForm({
+  form: 'organizerProfileForm'
+})(OrganizerProfile);
