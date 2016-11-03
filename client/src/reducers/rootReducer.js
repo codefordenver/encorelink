4bf19dbb2e9426c5152b63555bced79fd8a5f1e4
Darren Richmond <darrenrichmond@gmail.com>
2016-11-03 15:56:05 -0600

    Added descriptive comments for the combineReducers convenience functioin

--- client/src/reducers/rootReducer.js
+++ client/src/reducers/rootReducer.js
@@ -4,6 +4,13 @@ import { reducer as formReducer } from 'redux-form';
 import userManager from './userManager';
 import eventManager from './eventManager';
 
+/**
+ * We use this convenience function from Redux to allow us to combine all the
+ * reducers into one big reducer that handles all of our state, while allowing
+ * us to keep the reducers in separate files for better organization. We've defined
+ * the userManager and eventManager reducers (in this same folder), and we get the
+ * formReducer from redux-form.
+ */
 const encoreLinkReducer = combineReducers({
   userManager,
   eventManager,
