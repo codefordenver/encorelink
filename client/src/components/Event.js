35f217b3f566ecdd9921fff874169e1608602819
Agustin Vijoditz <avijoditz@wondersign.com>
2016-11-12 21:46:49 -0700

    Header logo linked to landing or events // Link to Events from event page

--- client/src/components/Event.js
+++ client/src/components/Event.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import { getFormattedDayAndTime } from '../utils/dateFormatting';
+import { Link } from 'react-router';
 
 class Event extends React.Component {
   static propTypes = {
@@ -34,6 +35,10 @@ class Event extends React.Component {
           <button className="button" onClick={() => this.props.signUpForEvent(this.props.event)}>
             Sign Up
           </button> }
+        <br />
+        <br />
+        <br />
+        <Link to="/events">Back to Events</Link>
       </div>
     );
   }
