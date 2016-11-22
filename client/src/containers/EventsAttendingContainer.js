78f70f9a9a74ee9df24ece2b7f07ee2fdfd20f7f
Nathan Kluth <nlkluth@gmail.com>
2016-11-21 20:34:29 -0700

    remove create event button from eventsattending

--- client/src/containers/EventsAttendingContainer.js
+++ client/src/containers/EventsAttendingContainer.js
@@ -1,6 +1,12 @@
 import Events from '../components/Events';
 import gimmeData from '../utils/gimmeData';
-import { getUserId } from '../reducers/userReducer';
+import { isMusician, getUserId } from '../reducers/userReducer';
+
+function mapStateToProps(state) {
+  return {
+    isMusician: isMusician(state)
+  };
+}
 
 const getEventsAttendingUrl = (state) => `users/${getUserId(state)}/eventsAttending`;
-export default gimmeData(getEventsAttendingUrl)(Events);
+export default gimmeData(getEventsAttendingUrl, mapStateToProps)(Events);
