b7df79f5e78194d952fb048977911a2cdeb4e6e4
Nathan Kluth <nlkluth@gmail.com>
2016-12-12 20:51:30 -0700

    working on create route for organization - on success eventActions getting the users organization not working

--- client/src/actions/eventActions.js
+++ client/src/actions/eventActions.js
@@ -1,7 +1,8 @@
 import { browserHistory } from 'react-router';
 import { createApiAction, createAction, createErrorAction } from '../utils/reduxActions';
 import { put } from '../utils/apiHelpers';
-import { getUserId, getUser } from '../reducers/userReducer';
+import { getUserId } from '../reducers/userReducer';
+import { getModels } from '../reducers/modelsReducer';
 import {
   SIGNUP_FOR_EVENT_FAILURE,
   SIGNUP_FOR_EVENT_SUCCESS,
@@ -15,7 +16,7 @@ export function createEvent(formData) {
     body: correctDatesForKeys(formData, ['date', 'endDate']),
 
     onSuccess: (res, state) => {
-      const organizations = getUser(state).organizations || [];
+      const organizations = getModels(state, `/users/${getUserId(state)}/organization`) || [];
       if (organizations.length) {
         browserHistory.push('/events');
         return;
