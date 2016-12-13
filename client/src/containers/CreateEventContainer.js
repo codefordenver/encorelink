efcff9f3886019fd2d442e2813850f97b9a6d507
Nathan Kluth <nlkluth@gmail.com>
2016-12-12 18:52:50 -0700

    fetch organizations and redirect after event create

--- client/src/containers/CreateEventContainer.js
+++ client/src/containers/CreateEventContainer.js
@@ -1,10 +1,14 @@
-import { connect } from 'react-redux';
+import gimmeData from '../utils/gimmeData';
 import { createEvent } from '../actions/eventActions';
-
 import CreateEvent from '../components/CreateEvent';
+import { getUserId } from '../reducers/userReducer';
+
+function urlFn(state) {
+  return `users/${getUserId(state)}/organization`;
+}
 
 const mapDispatchToProps = {
   onSubmit: createEvent
 };
 
-export default connect(null, mapDispatchToProps)(CreateEvent);
+export default gimmeData(urlFn, null, mapDispatchToProps)(CreateEvent);
