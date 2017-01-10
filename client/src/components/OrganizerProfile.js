ecf41ea6fca8f9ad66f8109c576ac33fc0e4f3ab
Nathan Kluth <nlkluth@gmail.com>
2017-01-09 19:06:43 -0700

    adding update for organization

--- client/src/components/OrganizerProfile.js
+++ client/src/components/OrganizerProfile.js
@@ -1,145 +1,152 @@
 import React, { PropTypes } from 'react';
 import { Field, reduxForm } from 'redux-form';
 
-const OrganizerProfile = ({ handleSubmit }) => (
-  <div className="row">
-    <div className="small-12 columns">
-      <h1>Create Your Profile</h1>
-      <h5 className="subheader">
-        Your profile provides information we can share with musicians so they know about you and your organization.
-      </h5>
-      <br />
-      <form className="form-create-event" onSubmit={handleSubmit}>
-        <div className="row">
-          <div className="small-12 medium-12 columns">
-            <label>Organization name:
-              <Field
-                name="name"
-                component="input"
-                type="text"
-                placeholder="Organization name"
-                required
-                autoFocus
-              />
-            </label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="small-12 medium-12 columns">
-            <label>Your name:
-              <input type="text" required />
-            </label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="small-12 medium-12 columns">
-            <label>Job title:
-              <input type="text" required />
-            </label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="small-12 medium-12 columns">
-            <label>Address:
-              <Field
-                name="address"
-                component="input"
-                type="text"
-                placeholder="Address"
-                required
-              />
-            </label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="small-12 medium-12 columns">
-            <label>City:
-              <input type="text" required />
-            </label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="small-12 medium-7 columns">
-            <div className="row">
-              <div className="small-12 medium-12 columns">
-                <label>State:
-                  <input type="text" required />
-                </label>
-              </div>
-            </div>
-          </div>
-          <div className="small-12 medium-3 columns">
-            <div className="row">
-              <div className="small-12 medium-12 columns">
-                <label>Zipcode:
-                  <input type="text" required />
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="small-12 medium-12 columns">
-            <label>Email:
-              <input type="text" required />
-            </label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="small-12 medium-7 columns">
-            <div className="row">
-              <div className="small-12 medium-12 columns">
-                <label>Phone:
-                  <input type="text" required />
-                </label>
-              </div>
-            </div>
-          </div>
-          <div className="small-12 medium-2 columns">
-            <div className="row">
-              <div className="small-12 medium-12 columns">
-                <label>Ext:
-                  <input type="text" />
-                </label>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="row">
-          <div className="small-12 medium-12 columns">
-            <label>Best contact method (email is default):
-              <input type="text" />
-            </label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="small-12 medium-12 columns">
-            <label>Special notes:
-              <textarea />
-            </label>
-          </div>
-        </div>
-        <div className="row">
-          <div className="small-12 medium-12 columns">
-            <label>Does the facility have a piano or any other instruments:</label>
-            <input type="radio" name="hasInstruments" value="piano" id="hasInstrumentsPiano" /><label htmlFor="hasInstrumentsPiano">Piano</label>
-            <input type="radio" name="hasInstruments" value="no" id="hasInstrumentsNo" /><label htmlFor="hasInstrumentsNo">Not yet</label>
-            <input type="radio" name="hasInstruments" value="other" id="hasInstrumentsOther" /><label htmlFor="hasInstrumentsOther">Other (please specify in special notes above)</label>
+const OrganizerProfile = ({ handleSubmit, createOrganization, updateOrganization, data }) => {
+  const updatedOrganization = (formData) => updateOrganization(formData, data[0].id);
 
+  return (
+    <div className="row">
+      <div className="small-12 columns">
+        <h1>Create Your Profile</h1>
+        <h5 className="subheader">
+          Your profile provides information we can share with musicians so they know about you and your organization.
+        </h5>
+        <br />
+        <form className="form-create-event" onSubmit={handleSubmit(data.length ? updatedOrganization : createOrganization)}>
+          <div className="row">
+            <div className="small-12 medium-12 columns">
+              <label>Organization name:
+                <Field
+                  name="name"
+                  component="input"
+                  type="text"
+                  placeholder="Organization name"
+                  required
+                  autoFocus
+                />
+              </label>
+            </div>
           </div>
-        </div>
-        <div className="row">
-          <div className="small-12 medium-12 columns">
-            <button type="submit" className="button">Submit</button>
+          <div className="row">
+            <div className="small-12 medium-12 columns">
+              <label>Your name:
+                <input type="text" required />
+              </label>
+            </div>
           </div>
-        </div>
-      </form>
+          <div className="row">
+            <div className="small-12 medium-12 columns">
+              <label>Job title:
+                <input type="text" required />
+              </label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="small-12 medium-12 columns">
+              <label>Address:
+                <Field
+                  name="address"
+                  component="input"
+                  type="text"
+                  placeholder="Address"
+                  required
+                />
+              </label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="small-12 medium-12 columns">
+              <label>City:
+                <input type="text" required />
+              </label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="small-12 medium-7 columns">
+              <div className="row">
+                <div className="small-12 medium-12 columns">
+                  <label>State:
+                    <input type="text" required />
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="small-12 medium-3 columns">
+              <div className="row">
+                <div className="small-12 medium-12 columns">
+                  <label>Zipcode:
+                    <input type="text" required />
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="small-12 medium-12 columns">
+              <label>Email:
+                <input type="text" required />
+              </label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="small-12 medium-7 columns">
+              <div className="row">
+                <div className="small-12 medium-12 columns">
+                  <label>Phone:
+                    <input type="text" required />
+                  </label>
+                </div>
+              </div>
+            </div>
+            <div className="small-12 medium-2 columns">
+              <div className="row">
+                <div className="small-12 medium-12 columns">
+                  <label>Ext:
+                    <input type="text" />
+                  </label>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="row">
+            <div className="small-12 medium-12 columns">
+              <label>Best contact method (email is default):
+                <input type="text" />
+              </label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="small-12 medium-12 columns">
+              <label>Special notes:
+                <textarea />
+              </label>
+            </div>
+          </div>
+          <div className="row">
+            <div className="small-12 medium-12 columns">
+              <label>Does the facility have a piano or any other instruments:</label>
+              <input type="radio" name="hasInstruments" value="piano" id="hasInstrumentsPiano" /><label htmlFor="hasInstrumentsPiano">Piano</label>
+              <input type="radio" name="hasInstruments" value="no" id="hasInstrumentsNo" /><label htmlFor="hasInstrumentsNo">Not yet</label>
+              <input type="radio" name="hasInstruments" value="other" id="hasInstrumentsOther" /><label htmlFor="hasInstrumentsOther">Other (please specify in special notes above)</label>
+
+            </div>
+          </div>
+          <div className="row">
+            <div className="small-12 medium-12 columns">
+              <button type="submit" className="button">Submit</button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
 OrganizerProfile.propTypes = {
-  handleSubmit: PropTypes.func.isRequired
+  handleSubmit: PropTypes.func.isRequired,
+  createOrganization: PropTypes.func.isRequired,
+  updateOrganization: PropTypes.func.isRequired,
+  data: PropTypes.array
 };
 
 export default reduxForm({
--- client/src/containers/OrganizerProfileContainer.js
+++ client/src/containers/OrganizerProfileContainer.js
@@ -1,14 +1,27 @@
 import OrganizerProfile from '../components/OrganizerProfile';
-import { createOrganization } from '../actions/organizationActions';
+import { createOrganization, updateOrganization } from '../actions/organizationActions';
 import { getUserId } from '../reducers/userReducer';
 import gimmeData from '../utils/gimmeData';
+import { getModels } from '../reducers/modelsReducer';
 
 function urlFn(state) {
   return `users/${getUserId(state)}/organization`;
 }
 
 const mapDispatchToProps = {
-  onSubmit: createOrganization
+  createOrganization,
+  updateOrganization
 };
 
-export default gimmeData(urlFn, mapDispatchToProps, mapDispatchToProps)(OrganizerProfile);
+function getInitialValues(state) {
+  const organizations = getModels(state, urlFn(state)) || [];
+  return organizations[0];
+}
+
+function mapStateToProps(state) {
+  return {
+    initialValues: getInitialValues(state)
+  };
+}
+
+export default gimmeData(urlFn, mapStateToProps, mapDispatchToProps)(OrganizerProfile);
