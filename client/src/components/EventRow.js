8e00a172b62f75c249e2a7af9fd5470f23051c81
ShannonB <tarislar@gmail.com>
2017-04-10 19:23:56 -0600

    First commit - and branch prep

--- client/src/components/EventRow.js
+++ client/src/components/EventRow.js
@@ -19,6 +19,7 @@ function EventRow({ event }) {
       <div className="small-12 medium-2 columns">
         <Link to={`/event/${id}`}>details</Link>
       </div>
+      //TODO add approved/pending notification here.
     </div>
   );
 }
