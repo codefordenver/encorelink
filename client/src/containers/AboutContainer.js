c12f8426a165d10357576a78615619a9f235a877
Gray Smith <gray.smith00@gmail.com>
2018-03-05 21:55:40 -0500

    added copy, code fore denver link, and photo to about us page, plus a small edit in the copy

--- client/src/containers/AboutContainer.js
+++ client/src/containers/AboutContainer.js
@@ -5,7 +5,7 @@ function AboutContainer() {
     <div className="row">
       <div className="small-10">
         <h1>About Us</h1>
-        <p>EncoreLink is a platform dedicated to linking musicians who are willing to volunteer their time with organizations who have people who would greatly benefit from that experience. Encore Link is created and maintained by Code for Denver, a volunteer-driven community outreach that is commited to improving lives through technology. More information about Code For Denver and some of their other volunteer projects can be found at <a href="https://www.codefordenver.org/" target="_blank">codefordenver.org</a>.</p>
+        <p>EncoreLink is a platform dedicated to linking musicians who are willing to volunteer their time with organizations who have people who would greatly benefit from that experience. Encore Link is created and maintained by Code for Denver, a volunteer-driven community outreach that is commited to improving lives through technology. More information about Code for Denver and some of their other volunteer projects can be found at <a href="https://www.codefordenver.org/" target="_blank">codefordenver.org</a>.</p>
         <img src="./public/img/IMG_8306.jpg"></img>
       </div>
     </div>
