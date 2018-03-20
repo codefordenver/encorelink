ef778c6b2691a0e4a6743b1f75ca739b4648a878
Gray Smith <gray.smith00@gmail.com>
2018-03-19 22:10:24 -0600

    further modified about us page using Julia's copy and two pictures, added an about.scss file for styling

--- client/src/containers/AboutContainer.js
+++ client/src/containers/AboutContainer.js
@@ -5,8 +5,23 @@ function AboutContainer() {
     <div className="row">
       <div className="small-10">
         <h1>About Us</h1>
-        <p>EncoreLink is a platform dedicated to linking musicians who are willing to volunteer their time with organizations who have people who would greatly benefit from that experience. Encore Link is created and maintained by Code for Denver, a volunteer-driven community outreach that is commited to improving lives through technology. More information about Code for Denver and some of their other volunteer projects can be found at <a href="https://www.codefordenver.org/" target="_blank">codefordenver.org</a>.</p>
-        <img src="./public/img/IMG_8306.jpg"></img>
+        <hr/>
+        <div className="row about-img">
+          <div className="medium-6 small-12 columns">
+            <img src="./public/img/band-nursinghome.jpg"></img>
+          </div>
+          <div className="medium-6 small-12 columns">
+            <img src="./public/img/IMG_8306.jpg"></img>
+          </div>
+        </div>
+        
+        <p><strong>Mission: </strong>EncoreLink is a web platform that simplifies the process to schedule events at healthcare facilities in order to increase the number of volunteer musical performances.</p> 
+        
+        <p><strong>Vision: </strong>The name “EncoreLink” is a reflection of our mission and vision that the musicians keep coming back and play more encores through the link between the musicians and the facilities.</p>
+          
+        <p><strong>Major Partners: </strong>Encore Link is created and currently maintained by <a href="https://www.codefordenver.org/" target="_blank">Code for Denver</a>, a volunteer-driven community outreach that is committed to improving lives through technology. Many current students and graduates of the <a href="https://www.du.edu/lamont" target="_blank">Lamont School of Music</a> have volunteered through individual community outreach and now they are helping us launch EncoreLink in Denver, Colorado. Many thanks to the volunteers at Code for Denver for helping us build this incredible web platform!</p>
+          
+        
       </div>
     </div>
   );
