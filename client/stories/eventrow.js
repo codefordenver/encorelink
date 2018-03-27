364c9f47b46dfd4688bd30a108b557b0055c899e
Anna Lewis <anna.noel.lewis@gmail.com>
2018-03-26 19:09:23 -0600

    Eventrow storybook knobs (#365)
    * add knobs to eventrow in storybook
    
    * change time for tests

--- client/stories/eventrow.js
+++ client/stories/eventrow.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@kadira/storybook';
-import { withKnobs, boolean } from '@kadira/storybook-addon-knobs';
+import { withKnobs, boolean, date, text } from '@kadira/storybook-addon-knobs';
 import EventRow from '../src/components/EventRow';
 
 storiesOf('EventRow', module)
@@ -12,11 +12,11 @@ storiesOf('EventRow', module)
       <EventRow
         hideCalendar={boolean('hide calendar', false)}
         event={{
-          date: '2018-01-01',
-          endDate: '2018-01-02',
+          date: date('data.date', new Date('Jan 1, 2018 12:00 am GMT')),
+          endDate: date('data.endDate', new Date('Jan 1, 1901 12:00 am GMT')),
           id: 5,
-          location: 'denver',
-          name: 'music' }}
+          location: text('data.location', 'denver'),
+          name: text('data.name', 'music') }}
       />
     )
   );
